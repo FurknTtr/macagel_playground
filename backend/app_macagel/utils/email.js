aeaff4cbed9df5f56c4a9ab8bca46f49b7aa7441
Furkn <furkn.ttr@gmail.com>
2026-04-04 23:17:12 +0300

    mail portu 465 ypaıldı

--- backend/app_macagel/utils/email.js
+++ backend/app_macagel/utils/email.js
@@ -2,14 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT, 10),
-    secure: false,
-    requireTLS: true,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.trim(),
     },
 });
+
 
 const mailService = {
     // Hoşgeldiniz Emaili (Kayıt sonrası)
