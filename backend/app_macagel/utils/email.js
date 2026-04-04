790a14da15c44276ec02058b65fa07414c22e451
Furkn <furkn.ttr@gmail.com>
2026-04-05 00:14:19 +0300

    587 portuna geri dönüş

--- backend/app_macagel/utils/email.js
+++ backend/app_macagel/utils/email.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT, 10),
-    secure: true,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.trim(),
